d8f5045d320857b8d64abc85a0acbc666737c3b2
mikewm2001 <michaelwmei09@gmail.com>
2024-08-11 22:11:23 -0400

    Added depth variable to reduce overhead on thread creation

--- src/sort.h
+++ src/sort.h
@@ -2,6 +2,6 @@
 #define SORT_H
 
 void quicksort(int arr[], int low, int high);
-void quicksort_parallel(int arr[], int low, int high);
+void quicksort_parallel(int arr[], int low, int high, int depth = 0);
 
 #endif
